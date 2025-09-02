d0d349e34522fe2bd208283ddbf7118c9fe4747b
Anderson <Anderson@DESKTOP-N3N6QSB>
2025-09-02 19:12:00 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSoundAPI/Program.cs
@@ -0,0 +1,27 @@
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => 
+                   options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+var app = builder.Build();
+
+app.MapGet("/artistas", () =>{
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/artistas/{nome}", ( string nome) => {
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    var artista = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+
+    if (artista is null)
+        return Results.NotFound();
+
+    return Results.Ok(artista);
+
+
+});
+
+app.Run();
